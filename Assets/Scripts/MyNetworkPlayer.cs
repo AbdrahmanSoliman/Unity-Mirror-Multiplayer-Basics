75bea8f8141e8e391991cfe2e58ea73bf4628cd5
Abdelrahman A. Soliman <43556094+AbdrahmanSoliman@users.noreply.github.com>
2023-03-07 03:06:59 +0200

    basics
    added the multiplayer basics of using Mirror networking solution and running on Unity engine

---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
+    [SerializeField]
+    private string displayName = "Missing Name!";
+
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))]
+    [SerializeField]
+    private Color displayColor = Color.black;
+
+    #region Server
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetDisplayColor(Color newDisplayColor)
+    {
+        displayColor = newDisplayColor;
+    }
+
+    [Command]
+    public void CmdSetDisplayName(string newDisplayName)
+    {
+        if(newDisplayName.Length < 2 || newDisplayName.Length > 20) return;
+        
+        RpcLogNewName(newDisplayName);
+
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region Client
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    [ContextMenu("Set New Name")]
+    public void SetName()
+    {
+        CmdSetDisplayName("New Player Name");
+    }
+
+    [ClientRpc] 
+    public void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
+}
